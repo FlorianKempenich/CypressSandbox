c14b2ebd9dffc54e05a93d8bbe15f3be4ab230a3
Florian Kempenich <Flori@nKempenich.com>
2020-09-30 12:59:25 +0100

    Add blink-diff sandbox

---
+++ blinkDiffSandbox/blinkDiff.js
@@ -0,0 +1,30 @@
+const BlinkDiff = require("blink-diff");
+
+function diffBetween(img1Id, img2Id) {
+  const percent = (pct) => pct / 100;
+
+  const diff = new BlinkDiff({
+    imageAPath: "./" + img1Id + ".png",
+    imageBPath: "./" + img2Id + ".png",
+    blockOut: { x: 100, y: 100, width: 200, height: 100 },
+    // cropImageA: { x:200, y:300, width:2000, height:1000 },
+    thresholdType: BlinkDiff.THRESHOLD_PERCENT,
+    threshold: percent(1),
+    imageOutputPath: `diffBetween_${img1Id}-${img2Id}.png`,
+    debug: true,
+  });
+
+  diff.run(function (error, result) {
+    if (error) {
+      throw error;
+    } else {
+      console.log(result)
+      console.log(diff.hasPassed(result.code) ? "Passed" : "Failed");
+      console.log("Found " + result.differences + " differences.");
+    }
+  });
+}
+
+
+diffBetween('square', 'squareCopy')
+diffBetween('square', 'squareModified')
